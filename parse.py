e99477debe637c9d5b18c2e2f4fad6069d3a062e
Michael Tayrov <mtayrov@gmail.com>
2020-02-16 04:36:28 +0300

    Parsing Houses Addresses

--- parse.py
+++ parse.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 from requests import Session
-from bs4 import BeautifulSoup
-from openpyxl import Workbook, load_workbook
 import pandas as pd
 
 # Territories URL
@@ -23,27 +21,19 @@ import pandas as pd
 
 class Web:
     default_origin = 'https://dom.gosuslugi.ru/'
-    territories_url = 'https://dom.gosuslugi.ru/interactive-reports/api/rest/services/housesConditionReport' \
-                      '/deteriorationTerritories'
-    houses_url = 'https://dom.gosuslugi.ru/interactive-reports/api/rest/services/housesConditionReport/houses'
 
-    def __init__(self, headers=None, origin=None, url=None,
+    def __init__(self, headers=None, origin=None,
                  user_agent='Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                            'Chrome/76.0.3809.132 Safari/537.36',
-                 debug=False
-                 ):
+                            'Chrome/76.0.3809.132 Safari/537.36'):
         self.headers = headers
         self.origin = self.default_origin if origin is None else origin
-        self.url = self.territories_url if url is None else url
         self.user_agent = user_agent
-        self.debug = debug
         self.s = Session()
         self.project_dir_path = Path(__file__).parent
         self.project_results_dir = self.project_dir_path / 'out'
         Path(self.project_results_dir).mkdir(exist_ok=True)
 
-    def load_data(self, url=None, **kwargs):
-        url = self.url if url is None else url
+    def load_data(self, url, **kwargs):
         site = self.s.get(self.origin, verify=True)
         cookies = dict(site.cookies)
         if self.headers:
@@ -55,6 +45,74 @@ class Web:
     def save_json(data, save_path='out.json', mode='w'):
         with open(save_path, mode) as f:
             f.write(data)
+
+
+class Territories:
+    def __init__(self, url=None, payload=None):
+        territories_payload = {
+            "managementTypes": [],
+            "operationYearFrom": 1799,
+            "operationYearTo": 2020,
+            "territories": [],
+            "withFederalDistricts": False,
+            "territoryCategory": "ADMINISTRATIVE"
+        }
+        territories_url = 'https://dom.gosuslugi.ru/interactive-reports/api/rest/services/housesConditionReport' \
+                          '/deteriorationTerritories'
+        self.url = territories_url if url is None else url
+        self.payload = territories_payload if payload is None else payload
+
+    @staticmethod
+    def dataframe(data: dict):
+        dataframe = pd.DataFrame.from_dict(data[0]['children'])
+        print('DF\n', dataframe.head())
+        print('DF\n', dataframe.info())
+        territories = dataframe['territory'].apply(pd.Series)
+        territories.columns = ['key'] + ['territory_' + col for col in territories.columns[1:]]
+        houses_with_deterioration = dataframe['housesWithDeterioration'].apply(pd.Series)
+        houses_with_deterioration.columns = ['housesWithDeterioration_' + col for col in
+                                             houses_with_deterioration.columns]
+        df = pd.concat([territories, houses_with_deterioration, dataframe], axis=1). \
+            drop(['territory', 'housesWithDeterioration'], axis=1)
+        df.set_index(['key'], inplace=True)
+        return dataframe
+
+
+class HouseAddresses:
+
+    def __init__(self, url=None, payload=None):
+        houses_url = 'https://dom.gosuslugi.ru/interactive-reports/api/rest/services/housesConditionReport/houses'
+        central_district_payload = {
+            "managementTypes": [],
+            "operationYearFrom": 1700,
+            "operationYearTo": 2020,
+            "territories": ["1"],
+            "withFederalDistricts": True,
+            "territoryCategory": "ADMINISTRATIVE"
+        }
+        houses_payload = {
+            "managementTypes": [],
+            "operationYearFrom": 1700,
+            "operationYearTo": 2020,
+            "territories": ["50"],
+            "withFederalDistricts": True,
+            "territoryCategory": "ADMINISTRATIVE",
+            "sortBy": "address",
+            "sortAsc": True,
+            "pageNumber": 1,
+            "pageSize": 67700
+        }
+        self.url = houses_url if url is None else url
+        self.payload = houses_payload if payload is None else payload
+        self.test_payload = central_district_payload
+
+    @staticmethod
+    def dataframe(data: dict):
+        dataframe = pd.DataFrame.from_dict(data['items'])
+        print('Prepare H-DF\n', dataframe.head())
+        print('Prepare H-DF\n', dataframe.info())
+        dataframe.set_index(['houseId'], inplace=True)
+        return dataframe
 
 
 def time_spent(func):
@@ -75,45 +133,33 @@ def time_spent(func):
 
 @time_spent
 def main():
-    territories_payload = {
-        "managementTypes": [],
-        "operationYearFrom": 1799,
-        "operationYearTo": 2020,
-        "territories": [],
-        "withFederalDistricts": False,
-        "territoryCategory": "ADMINISTRATIVE"
-    }
-    houses_payload = {
-        "managementTypes": [],
-        "operationYearFrom": 1700,
-        "operationYearTo": 2020,
-        "territories": ["50"],
-        "withFederalDistricts": True,
-        "territoryCategory": "ADMINISTRATIVE",
-        "sortBy": "address",
-        "sortAsc": True,
-        "pageNumber": 1,
-        "pageSize": 100
-    }
     w = Web()
-    territories_data = w.load_data(json=territories_payload).json()
-    print('Terr Data', territories_data)
-    w.save_json(str(territories_data), w.project_results_dir / 'out.json')
-    df = pd.DataFrame.from_dict(territories_data[0]['children'])
-    print('DF\n', df.head())
-    print('DF\n', df.info())
-    territories = df['territory'].apply(pd.Series)
-    territories.columns = ['key'] + ['territory_' + col for col in territories.columns[1:]]
-    houses_with_deterioration = df['housesWithDeterioration'].apply(pd.Series)
-    houses_with_deterioration.columns = ['housesWithDeterioration_' + col for col in houses_with_deterioration.columns]
-    df = pd.concat([territories, houses_with_deterioration, df], axis=1).\
-        drop(['territory', 'housesWithDeterioration'], axis=1)
-    df.set_index(['key'], inplace=True)
-    print('DF\n', df.head())
-    print('DF\n', df.info())
-    df.to_excel(w.project_results_dir / 'territories.xlsx')
+    t = Territories()
+    h = HouseAddresses()
 
-    # houses_data = w.load_data(json=houses_payload).json()
+    territories_data = w.load_data(url=t.url, json=t.payload).json()
+    w.save_json(str(territories_data), w.project_results_dir / 'territories.json')
+    print('Terr Data', territories_data)
+
+    terr_df = t.dataframe(territories_data)
+    terr_df.to_excel(w.project_results_dir / 'territories.xlsx')
+    print('DF\n', terr_df.head())
+    print('DF\n', terr_df.info())
+
+    # test_get = w.s.get('https://dom.gosuslugi.ru/#!/houses-condition/deterioration')
+    # print('Test Get: \n', test_get.content.decode('utf-8'))
+
+    central_district_data = w.load_data(t.url, json=h.test_payload).json()
+    print('Test Data', central_district_data)
+
+    houses_data = w.load_data(h.url, json=h.payload).json()
+    w.save_json(str(houses_data), w.project_results_dir / 'houses.json')
+    print('Houses Data', houses_data)
+
+    houses_df = h.dataframe(houses_data)
+    houses_df.to_excel(w.project_results_dir / 'houses.xlsx')
+    print('H-DF\n', houses_df.head())
+    print('H-DF\n', houses_df.info())
 
     w.s.close()
 
